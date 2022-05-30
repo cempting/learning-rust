2d5a8e36b772274327d9ff7e5b0db604a5c4e557
Christian Empting <Christian.Empting@zuehlke.com>
2022-05-30 11:22:18 +0200

    adds arguments to app

--- rng_daily/src/main.rs
+++ rng_daily/src/main.rs
@@ -6,12 +6,27 @@ mod names;
 mod display;
 mod font;
 
-//use clap::Parser;
+use clap::Parser;
+
+/// Daily organizer with randomized list
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the peoples file
+    #[clap(short, long)]
+    people: String,
+
+    /// Countdown timer value in seconds
+    #[clap(short, long, default_value_t = 60)]
+    countdown: u64,   
+}
 
 fn main() {
-    let filename = "./src/names/people.txt";
-    let names : names::Names = names::load_shuffled_names(filename);
-    let duration : Duration = Duration::from_secs(3);
+    let args = Args::parse();
+
+    //let filename = "./src/names/people.txt";
+    let names : names::Names = names::load_shuffled_names(args.people.as_str());
+    let duration : Duration = Duration::from_secs(args.countdown);
     let font = font::Font::new();
     
     for name in names.iter() {
